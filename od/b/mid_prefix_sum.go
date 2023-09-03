94569046d0b888d96850d20049bdedd8aeddddf0
Ben.Zheng <bengzheng.6dev9527@outlook.com>
2023-09-03 15:29:45 +0800

    Added solutions for od oj prefix sum.

--- od/b/mid_prefix_sum.go
+++ od/b/mid_prefix_sum.go
@@ -23,3 +23,37 @@ func JudgeContinuousSumIsMultipleOfK(nums []int, m int) int {
 	}
 	return 0
 }
+
+// 给定一个小写字母组成的字符串 s，请找出字符串中两个不同位置的字符作为分割点，使得字符串分成三个连续子串且子串权重相等，
+// 注意子串不包含分割点。
+// 若能找到满足条件的两个分割点，请输出这两个分割点在字符串中的位置下标，若不能找到满足条件的分割点请返回0,0。
+// 子串权重计算方式为:子串所有字符的ASCII码数值之和。
+// 输入为一个字符串，字符串由a~z，26个小写字母组成，5 ≤ 字符串长度 ≤ 200。
+
+func SameWeightSubContinuousStr(str string) [2]int {
+	ans := [2]int{0, 0}
+	prefixSum := make([]int, len(str)+1)
+	for i, s := range str {
+		prefixSum[i+1] = prefixSum[i] + int(s)
+
+	}
+
+	// i,j，i 不能从 0 开始，j 至少间隔 2
+	for i := 1; i < len(str); i++ {
+		weight1 := prefixSum[i] - prefixSum[0]
+		for j := i + 2; j < len(str); j++ {
+			weight2 := prefixSum[j] - prefixSum[i+1]
+			if weight1 < weight2 {
+				break
+			}
+			if weight1 == weight2 {
+				weight3 := prefixSum[len(str)] - prefixSum[j+1]
+				if weight2 == weight3 {
+					return [2]int{i, j}
+				}
+			}
+		}
+	}
+
+	return ans
+}
--- od/b/mid_prefix_sum_test.go
+++ od/b/mid_prefix_sum_test.go
@@ -1,6 +1,9 @@
 package b
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestJudgeContinuousSumIsMultipleOfK(t *testing.T) {
 	type args struct {
@@ -37,6 +40,39 @@ func TestJudgeContinuousSumIsMultipleOfK(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := JudgeContinuousSumIsMultipleOfK(tt.args.nums, tt.args.m); got != tt.want {
 				t.Errorf("JudgeContinuousSumIsMultipleOfK() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSameWeightSubContinuousStr(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [2]int
+	}{
+		{
+			name: "1",
+			args: args{
+				str: "acdbbbca",
+			},
+			want: [2]int{2, 5},
+		},
+		{
+			name: "2",
+			args: args{
+				str: "abcabc",
+			},
+			want: [2]int{0, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SameWeightSubContinuousStr(tt.args.str); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SameWeightSubContinuousStr() = %v, want %v", got, tt.want)
 			}
 		})
 	}
